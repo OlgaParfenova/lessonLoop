f08726a1b51c1ce40c4d1d7a8f5f72ab5f1ac4c9
Olga Parfenova <work.parfenova@gmail.com>
2023-02-22 21:42:59 +0300

    The second module is completed

--- script.js
+++ script.js
@@ -127,13 +127,130 @@
 //     }
 // }
 
+// task 17
 
+// let obj = {a: 1, b: 2, c: 3, d: 4, e: 5};
+//
+// for (let key in obj) {
+//     if (obj[key] % 2 === 1) {
+//         console.log(obj[key]);
+//     }
+// }
 
+// task 18
 
+// let res = 0
+//
+// for (let i = 2; i <= 100; i += 2) {
+//     res += i;
+// }
+//
+// console.log(res);
 
+// task 19
 
+// let res = 0;
+//
+// for (let i = 1; i <= 99; i += 2) {
+//     res += i;
+// }
+//
+// console.log(res);
 
+// task 20
 
+// let res = 1;
+//
+// for (let i = 1; i <= 20; i++) {
+//     res *= i;
+// }
+//
+// console.log(res);
 
+// task 21
+
+// let arr = [2, 5, 9, 3, 1, 4];
+// let res = 0;
+//
+// for (let elem of arr) {
+//     res += elem;
+// }
+//
+// console.log(res);
+
+// task 22
+
+// let arr = [2, 5, 9, 3, 1, 4];
+// let res = 0;
+//
+// for (let elem of arr) {
+//     if (elem % 2 ===0) {
+//         res += elem;
+//     }
+// }
+//
+// console.log(res);
+
+// task 23
+
+// let arr = [2, 5, 9, 3, 1, 4];
+// let res = 0;
+//
+// for (let elem of arr) {
+//     res += (elem ** 2);
+// }
+//
+// console.log(res);
+
+// task 24
+
+// let arr = [2, 5, 9, 3, 1, 4];
+// let res = 1;
+//
+// for (let i = 0; i < arr.length; i++) {
+//     res *= arr[i];
+// }
+//
+// console.log(res);
+
+// task 25
+
+// let str = '';
+//
+// for (let i = 0; i < 5; i++) {
+//     str += '-';
+// }
+//
+// console.log(str);
+
+// task 26
+
+// let str = '';
+//
+// for (let i = 1; i < 10; i++) {
+//     str += i;
+// }
+//
+// console.log(str);
+
+// task 27
+
+// let str = '';
+//
+// for (let i = 9; i > 0; i--) {
+//     str += i;
+// }
+//
+// console.log(str);
+
+// task 28
+
+// let str = '';
+//
+// for (let i = 1; i < 10; i++) {
+//     str += ('-'+ i);
+// }
+//
+// console.log(str);
 
 
