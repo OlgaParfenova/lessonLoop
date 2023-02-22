b95f516a0b235b631d3537403faf5f7f8a2b1f20
Olga Parfenova <work.parfenova@gmail.com>
2023-02-22 21:10:46 +0300

    The first module is completed

---
+++ script.js
@@ -0,0 +1,139 @@
+// task 1
+
+// for (let i = 1; i <= 100; i++) {
+//     console.log(i);
+// }
+
+// task 2
+
+// for (let i = 11; i <= 33; i++) {
+//     console.log(i);
+// }
+
+// task 3
+
+// for (let i = 0; i <= 100; i += 2) {
+//     console.log(i);
+// }
+
+// task 4
+
+// for (let i = 1; i <= 99; i += 2) {
+//     console.log(i);
+// }
+
+// task 5
+
+// for (let i = 100; i >= 0; i--) {
+//     console.log(i);
+// }
+
+//task 6
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+//
+// for (let element of arr) {
+//     console.log(element);
+// }
+
+// task 7
+
+// let obj = {x: 1, y: 2, z: 3};
+//
+// for (let key in obj) {
+//     console.log(key);
+// }
+
+// task 8
+
+// let obj = {x: 1, y: 2, z: 3};
+//
+// for (let key in obj) {
+//     console.log(obj[key]);
+// }
+
+// task 9
+
+// let i = 1;
+//
+// while (i <= 100) {
+//     console.log(i);
+//     i++;
+// }
+
+// task 10
+
+// let i = 11;
+//
+// while (i <= 33) {
+//     console.log(i);
+//     i++;
+// }
+
+// task 11
+
+// function multiplyByThree(num) {
+//     let counter = 0;
+//
+//     while (num <= 1000) {
+//         counter++;
+//         num *= 3;
+//     }
+//     console.log(num);
+//     console.log(counter)
+// }
+//
+// multiplyByThree(20);
+
+// task 12
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+//
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// task 13
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+//
+// for (let i = 1; i < arr.length - 1; i++) {
+//     console.log(arr[i]);
+// }
+
+// task 14
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+//
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     console.log(arr[i]);
+// }
+
+// task 15
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+//
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// task 16
+
+// let arr = [2, 5, 9, 15, 1, 4];
+//
+// for (let elem of arr) {
+//     if (elem > 3 && elem < 10) {
+//         console.log(elem);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
